4e13cd93827a2854b847254a02c2d365f36c21b7
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-07 14:23:23 -0500

    add syrupy for snapshots, parse feed from xml

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,11 @@
-from newsyacht import main
+from xml.etree import ElementTree
+
+from newsyacht import Feed, main
+
+
+def test_feed_from_xml(snapshot):
+    tree = ElementTree.parse("tests/fixtures/arch.xml")
+    assert Feed.from_xml(tree) == snapshot
 
 
 def test_main():
