08348b4f3999aaf60fd5d9a0af613d8049724345
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-13 14:41:23 -0500

    drop main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from xml.etree import ElementTree
 
-from newsyacht import Feed, load_urls, main
+from newsyacht import Feed, load_urls
 
 
 def test_feed_from_xml(snapshot):
@@ -10,7 +10,3 @@ def test_feed_from_xml(snapshot):
 
 def test_load_urls(snapshot):
     assert load_urls("tests/fixtures/urls") == snapshot
-
-
-def test_main():
-    main()
