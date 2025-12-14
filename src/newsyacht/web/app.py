718e96d7fae31877d8afe18d7027b34e92ed5ba6
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-13 21:27:27 -0500

    add css, show metadata, add mark-read button

--- src/newsyacht/web/app.py
+++ src/newsyacht/web/app.py
@@ -2,7 +2,7 @@ from operator import attrgetter
 from pathlib import Path
 
 from flask import Flask, redirect, render_template
-from newsyacht import Db
+from newsyacht import Db, DbItem
 
 
 class App:
@@ -16,11 +16,14 @@ class App:
         @self.app.route("/")
         def index():
             with Db(self.db) as db:
-                posts = sorted(
+                posts: list[DbItem] = sorted(
                     (post for post in db.get_posts() if post.link is not None),
                     key=attrgetter("date"),
                     reverse=True,
                 )
+                for post in posts:
+                    if post.inner.author is None:
+                        post.inner.author = db.get_feed_title(post.feed_id)
             unread_posts = [post for post in posts if not post.is_read]
             read_posts = [post for post in posts if post.is_read]
             return render_template(
@@ -33,6 +36,15 @@ class App:
                 db.set_read(item_id)
                 link = db.get_link(item_id)
             return redirect(link)
+
+        @self.app.route("/mark-read/<int:item_id>")
+        def mark_read(item_id):
+            "Like `App.read` but redirect to home without visiting the link"
+
+            with Db(self.db) as db:
+                db.set_read(item_id)
+
+            return redirect("/")
 
     def run(self, *args, **kwargs):
         self.app.run(*args, **kwargs)
