affd23cffde2452fd66a02bdbd09d08eee6c197c
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-27 09:15:41 -0500

    move load_urls to config.py

--- src/newsyacht/__init__.py
+++ src/newsyacht/__init__.py
@@ -19,11 +19,12 @@ from xml.etree.ElementTree import Element
 
 import httpx
 
+from .config import Color, Url, load_urls
+
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 
 FeedId = NewType("FeedId", int)
-Color = NewType("Color", str)
 Score = NewType("Score", float)
 
 
@@ -273,39 +274,6 @@ class Feed:
             description=find(root, "content"),
             items=items,
         )
-
-
-@dataclass
-class Url:
-    link: str
-    color: Color | None = None
-
-
-def load_urls(path) -> list[Url]:
-    """
-    Load a sequence of URLs from `path`, one per line.
-
-    Lines starting with `#` and empty lines are ignored.
-    """
-
-    urls = []
-    for line in Path(path).read_text().splitlines():
-        if line.startswith("#"):
-            continue
-
-        if len(stripped := line.strip()) == 0:
-            continue
-
-        match stripped.split():
-            case [url]:
-                urls.append(Url(url))
-            case [url, color]:
-                urls.append(Url(url, color))
-            case line:
-                msg = f"Unable to parse line: {line}"
-                raise ValueError(msg)
-
-    return urls
 
 
 def initial_score(count: int) -> float:
